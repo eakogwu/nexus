77c215787c856eb2983c8314e52dd585b0a75970
Emmanuel Akogwu <akogwuemmanuel57@gmail.com>
2021-12-24 22:40:13 -0600

    completed footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,83 @@
 import React from 'react';
+import styled from "styled-components";
+import { BsLinkedin, BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
+import {Link} from "react-router-dom";
 
-const Footer = () => {
+const Footer = (props) => {
+
   return (
-    <div>
-      
-    </div>
+    <FooterContainer>
+      <span>Copyright &copy; 2021 Nexus | Nursing. All rights reserved</span>
+      <ul className="links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/services">About</Link>
+        </li>
+        <li>
+          <Link to="/about">About us</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact us</Link>
+        </li>
+      </ul>
+      <ul className="social__links">
+        <li>
+          <BsFacebook />
+        </li>
+        <li>
+          <AiFillInstagram />
+        </li>
+        <li>
+          <BsLinkedin />
+        </li>
+      </ul>
+    </FooterContainer>
+
   );
-};
 
+}
 
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #002441;
+  color: #f1f1f1;
+  padding: 2.5rem;
+  ul {
+    display: flex;
+    list-style-type: none;
+    gap: 2rem;
+    li {
+      a {
+        text-decoration: none;
+        color: #f1f1f1;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #EEF8F3;
+        }
+      }
+      svg {
+        font-size: 1.3rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #EEF8F3;
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1024px) {
+    flex-direction: column;
+    gap: 2rem;
+    ul {
+      flex-direction: column;
+    }
+    .social__links {
+      flex-direction: row;
+    }
+  }
+`
 
 export default Footer;
